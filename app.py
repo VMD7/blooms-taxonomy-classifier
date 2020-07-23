ed84b6044f0eb99726fa2e7741c33d54972a77ba
Vijay Devane <62797043+VMD7@users.noreply.github.com>
2020-07-23 10:36:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import pickle
 import pandas as pd
 import nltk
-nltk.data.path.append('nltk_data/stropwords/english')
+nltk.download()
 from nltk.corpus import stopwords
 from nltk.tokenize import punkt
 from nltk.corpus.reader import wordnet
